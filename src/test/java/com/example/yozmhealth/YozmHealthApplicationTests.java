71153cf25e383f466d1b6993cb129a4a32c1f1a9
well0924 <well414965@gmail.com>
2024-09-10 20:26:14 +0900

    first commit

---
+++ src/test/java/com/example/yozmhealth/YozmHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yozmhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YozmHealthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
