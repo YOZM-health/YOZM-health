bfd498a12300e990adf40fcf909b93fdbe7ed05b
justlikesh <lpsac@naver.com>
2024-10-07 18:55:55 +0900

    Feat: 보안 설정 추가 및 인증/인가 정책 구성
    - SecurityFilterChain을 통해 인증 및 인가 정책 설정
      - 정적 리소스와 로그인, 회원가입 페이지는 모두 접근 허용
      - '/user', '/manager', '/admin' 페이지에 권한에 따른 접근 제한 추가
    - formLogin 설정에 성공 및 실패 핸들러와 AuthenticationDetailsSource 적용
    - REST API를 위한 별도의 SecurityFilterChain 추가
      - '/api/login' 경로에 대한 보안 정책 설정 및 CSRF 비활성화
    - 비밀번호 암호화 설정을 위한 PasswordEncoder Bean 추가

---
+++ src/main/java/com/example/yozmhealth/config/SecurityConfig.java
@@ -0,0 +1,81 @@
+package com.example.yozmhealth.config;
+
+import com.example.yozmhealth.handler.FormAccessDeniedHandler;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final AuthenticationProvider authenticationProvider;
+    private final AuthenticationSuccessHandler successHandler;
+    private final AuthenticationFailureHandler failureHandler;
+    private final AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {  // 여기서 별도의 설정을 할수잇다
+
+        http                                                     // 인증, 인가 정책을 설정할수잇다
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/css/**","/images/**","/js/**","/favicon.*","/*/icon-*").permitAll()
+                        .requestMatchers("/","signup","/login*").permitAll()
+                        .requestMatchers("/user").hasAuthority("ROLE_USER")
+                        .requestMatchers("/manager").hasAuthority("ROLE_MANAGER")
+                        .requestMatchers("/admin").hasAuthority("ROLE_ADMIN")
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/login").permitAll()
+                        .authenticationDetailsSource(authenticationDetailsSource)
+                        .successHandler(successHandler)
+                        .failureHandler(failureHandler)
+                )
+                .authenticationProvider(authenticationProvider)
+                .exceptionHandling(exception -> exception.accessDeniedHandler(new FormAccessDeniedHandler("/denied")))
+        ;
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain restSecurityFilterChain(HttpSecurity http, AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {  // 여기서 별도의 설정을 할수잇다
+
+//        AuthenticationManagerBuilder managerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+//        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+
+        http                                                 // 인증, 인가 정책을 설정할수잇다
+                .securityMatcher("/api/login")
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/css/**","/images/**","/js/**","/favicon.*","/*/icon-*").permitAll()
+                        .anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
+//                .addFilterBefore(restAuthenticationFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class)
+//                .authenticationManager(authenticationManager)
+        ;
+
+        return http.build();
+    }
+
+//    private RestAuthenticationFilter restAuthenticationFilter(AuthenticationManager authenticationManager){
+//        RestAuthenticationFilter restAuthenticationFilter = new RestAuthenticationFilter();
+//        restAuthenticationFilter.setAuthenticationManager(authenticationManager);
+//        return restAuthenticationFilter;
+//    }
+}
